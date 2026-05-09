6b9e571da07f8d0e970d9b2e849182e9aeae8f7b
sergioferreiraide-glitch <sergioferreira.ide@gmail.com>
2026-05-09 10:41:19 -0300

    CONFIGURAR APLICATIVO SERGIO

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/SERGIO-FERREIRA-DA-SILVA/',
+    base: '/SERGIO-FERREIRA-DA-SILVA/', // GitHub Pages Base Path
     plugins: [
       react(), 
       tailwindcss()
